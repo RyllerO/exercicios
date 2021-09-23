bbe3ccda11c3e12e4e2c8cc0cdf83f3df1a61e39
CITSMART\ryller.oliveira <ryller.oliveira@run2biz.com>
2021-09-23 18:52:39 -0300

    exercicios

---
+++ src/Arrays/No.java
@@ -0,0 +1,7 @@
+package Arrays;
+
+public class No {
+    public long no;
+    public No dir;
+    public No esq;
+}
---
+++ src/Arrays/Teste.java
@@ -0,0 +1,8 @@
+package Arrays;
+import java.nio.charset.Charset;
+
+public class Teste {
+    public static void main(String[] arg) {
+        System.out.println(Charset.defaultCharset());
+    }
+}
